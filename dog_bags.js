75b3cf3fa91ca0137a4eeb94911f72ca4601092f
maps <78662264+dogbags@users.noreply.github.com>
2022-08-21 16:16:53 +0100

    Add files via upload

---
+++ dog_bags.js
@@ -0,0 +1,26 @@
+var dog_bags = [
+[38.68916, -9.33915, "images/20211008_r-joao-da-silva-203_250x245.png", "Rua João da SIlva 203"],
+[38.68907, -9.34014, "images/20211103_r-joao-da-silva-302_359x249.png", "Rua João da SIlva 302"],
+[38.69352, -9.337782,
+"images/20211008_quinta-da-alagoa-sul_364x261.png", "Quinta da Alagoa, portão sul"],
+[ 38.69103, -9.334246,"images/20211020_jardim-julio-moreira_400x257.png", "Jardim Julio Moreira"],
+[38.691557, -9.339413 , "images/20211008_jardim-fernandes-nunes-bombeiros_410x330.png", "Jardim Fernandes Nunes<br>(Bombeiros)"],
+[38.693918, -9.343352, "images/20211008_r-do-tejo-parqueIInfantil_375x238.png", "R Do Tejo<br>(Parque infantil)"],
+[38.6931, -9.3408, "images/20211015_jardim-juiz-lopes-quadros_381x257.png", "Jardim Juiz Lopes Quadros"],
+[38.691782, -9.335075, "images/20211020_av-loureiro_400x257.png", "Av Loureiro"],
+[38.69236, -9.336319, "images/20211020_nossa-sra-dos-remedios_390x293.png", "Nossa Sra Dos Remedios"],
+[38.693134, -9.33471, "images/20211020_r-antonio-ugusto-de-carvalho_400x279.png", "R Antonio Augusto De Carvalho"],
+[38.688667, -9.335558, "images/20211103_r--5-de-outubro_420x250.png", "R  5 De Outubro"],
+[38.691008, -9.328696, "images/20211108_alto-dos-lombos-esquina-r-das-parreiras_420x281.png", "Alto Dos Lombos<br>(esquina R Das Parreiras)"],
+[38.691221, -9.329415, "images/20211108_alto-dos-lombos-esquina-r-do-parque_400x259.png", "Alto Dos Lombos<br> (esquina R Do Parque)"],
+[38.6871, -9.3416, "images/IMG_20220124_153221-ParqueDoJunqueiro_333x234.png", "Parque do Junqueiro"],
+[38.6843, -9.345, "images/IMG_20220124_154336-RuaLuandaEsquinaBartolomeuDias_302x233.png", "Rua de Luanda<br>(esquina R Bartolomeu Dias)"],
+[38.6842, -9.3396, "images/IMG_20220124-RLourençoMarques_274x203.png", "Rua de Lourenço Marques"],
+[38.6844, -9.3428, "images/IMG_20220124-PracetaJunqueiroNorte_315x233.png", "Praceta Junqueiro Norte"],
+[38.7023, -9.3256,  "images/20220818_180022_RMissionariosDoEspiritoSanto_Sassoeiros_360x260.png", "R. Missionários do Espírito Santo<br>Sassoeiros"],
+[38.7071, -9.3306,  "images/20220819_134219_AvRioDao_Arneiro_360x260.png", "R. Av. do Rio Dão, ao fundo<br>Arneiro"],
+];
+
+
+
+
